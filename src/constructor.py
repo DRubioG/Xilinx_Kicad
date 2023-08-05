17fd6ca0408a267567faa0f6fa42c9f071258d82
David R <davidrubio97@outlook.com>
2023-08-06 00:03:09 +0200

    upd

--- src/constructor.py
+++ src/constructor.py
@@ -76,25 +76,103 @@ def listado(pines):
             pin_bank[cont].append(pin)
         pin_bank_ant = pin[2]
 
-    cont = -1
-    for list in pin_bank:
-        lon = len(list)
-        if lon >= 100:
-            pass
+    bank_gnd = []
+    bank_mgtav = []
+    bank_ngnd = []
+    bank_ps_mgt = []
+    bank_vcc = []
+    bank_nc  =[]
+    pin_bank2 = []
+    for na in pin_bank:
+        if na[0][2] == "NA":
+            for na2 in na:
+                if na2[1][:3] == "GND":
+                    bank_gnd.append(na2)
+                elif na2[1][:5] == "MGTAV":
+                    bank_mgtav.append(na2)
+                elif na2[1][:6] == "PS_MGT":
+                    bank_ps_mgt.append(na2)
+                elif na2[1][:3] == "VCC":
+                    bank_vcc.append(na2)
+                elif na2[1][:2] == "NC":
+                    bank_nc.append(na2)
+                else:
+                    bank_ngnd.append(na2)
+            if bank_vcc:
+                pin_bank2.append(bank_vcc)
+            if bank_gnd:
+                pin_bank2.append(bank_gnd)
+            if bank_mgtav:
+                pin_bank2.append(bank_mgtav)
+            if bank_ngnd:
+                pin_bank2.append(bank_ngnd)
+            if bank_nc:
+                pin_bank2.append(bank_nc)
+        else:
+            pin_bank2.append(na)
+    
+    pin_bank3 = []
+    pin_gnd = []
+    for i in pin_bank2:
+        if i[0][1][:3] == "GND":
+            if len(i) > 100:
+                for t in i:
+                    pin_gnd.append(t)
+                    if len(pin_gnd) == 100:
+                        pin_bank3.append(pin_gnd)
+                        pin_gnd=[]
+                if pin_gnd:
+                    pin_bank3.append(pin_gnd)
+        else:
+            pin_bank3.append(i)
+    
+    return pin_bank3
+    # return lista, capas
+
+def generador_pines(pins):
+    
+
+    for pin in pins:
+        lon = int(len(pin)/2)
+        mitad = 0
+        max = 0
+
+        if lon >= 75:
+            mitad = 1
+
+        for l in pin:
+            rt = len(l[1])
+            if rt > max:
+                max = rt
+
+        if max < 19:
+            x = 650 -200      # check this numbers
+        else:
+            x = 1000    #check this numbers
+        
+        if mitad == 1:
+            u = int(lon/2)*100+200
+        else:
+            u = lon*100+200
+        
+
+        # agregar rectangulo
+        wr += create_square((str(x), str(u)), (str(-x), str(-u)))
+
+        # agregar pines
+        for i in pin:
+
+            wr += create_pin(i)
 
 
-
-    return lista, capas
-
-def generador_pines():
     pass
 
 def create_pin(name, number, bank):
-    wr = "\t\t\t(pin "
+    wr = "\t\t\t(pin " 
     #tipo de pin
-
+    wr += tipo 
     #posicion
-
+    wr += " line (at " + pos_x + " " + pos_y + " " + dir + ") (length 2.54)"
     #nombre
     wr += "\t\t\t\t(name \"" + name + "\" (effects (font (size 1.27 1.27))))\n"
     #numero
@@ -102,9 +180,7 @@ def create_pin(name, number, bank):
     wr += "\t\t\t)\n"
     return wr
 
-def create_square():
-    p1_pos = (a,b)
-    p2_pos = (c,d)
+def create_square(p1_pos, p2_pos):
 
     wr = "\t\t\t(rectangle (start " + p1_pos[0] + " " + p1_pos[1] + ") \
         (end " + p2_pos[0] + " " + p2_pos[1] + ")\n\
@@ -135,7 +211,7 @@ if __name__=="__main__":
 
         pines = get_pins(chip)
 
-        lista, capas = listado(pines)
+        lista = listado(pines)
         
 
         wr += "\t(symbol \"" + name + "\" (in_bom yes) (on_board yes)  \
@@ -155,7 +231,7 @@ if __name__=="__main__":
 \t\t\t(effects (font (size 1.27 1.27)))\n \
 \t\t)\n"
 # \t\t(symbol \"" + name + "_" + capa + "_1\" 
-        wr += generador_pines()
+        wr += generador_pines(lista)
     wr += "\t)\n)"
 
     f.close()
